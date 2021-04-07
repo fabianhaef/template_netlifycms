68f24bfe63c306d6d9444e35f6e62196d4d66460
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-04-07 22:08:06 +0200

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <Layout>
       <section className="">
+        <h1>Hello, World</h1>
         <h1 className="">Willkommen beim Schwingklub Zug und Umgebung</h1>
         <h2 className="">Seit XXX ... </h2>
       </section>
