8d7950ef680fc285c0cf3727cec97c83912b2f70
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-04-24 16:17:45 +0200

    added key

--- src/pages/bilder.js
+++ src/pages/bilder.js
@@ -2,19 +2,19 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import bilder from '../../public/data/bilder.json'
 
-export default function Gallery({ bilder = [] }) {
+export default function Bilder({ bilder = [] }) {
+  { console.log(bilder) }
   return (
     <Layout title="athlethen">
-      <h1 className="title">Unsere Athlethen</h1>
+      <h1 className="title">Gllaerie</h1>
       {bilder.map(bild => (
-        <div className="box">
-          <h1 className="title">{bild.title}</h1>
-          <em className="subtitle is-6 is-block">{bild.dateFormatted}</em>
-          <div className="content section" dangerouslySetInnerHTML={{ __html: bild.bodyHtml }} />
+        < div className="box" key={bild.title}>
+          { console.log(bild)}
           <img src={bild.coverImage} alt={bild.title} width="256" height="256"></img>
         </div>
-      ))}
-    </Layout>
+      ))
+      }
+    </Layout >
   )
 }
 
