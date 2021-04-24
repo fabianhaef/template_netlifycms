5ace8a59cb23e9235a007942f7c9a75fb91c7740
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-04-24 17:18:23 +0200

    config.yml

--- src/pages/schwingers/[slug].js
+++ src/pages/schwingers/[slug].js
@@ -6,7 +6,7 @@ const defaultCoverImage = '/img/34961490322_bb9611120a_k.jpg'
 
 export default function SchwingerPost({ schwinger }) {
   return (
-    <Layout title={`${schwinger.title} | Blog`}>
+    <Layout title={`${schwinger.title}`}>
       <Link href="/blog"><a className="button mb-4">◀️ All Entries</a></Link>
 
       <div className="cover-image" style={{ backgroundImage: `url(${schwinger.coverImage || defaultCoverImage})` }}>
