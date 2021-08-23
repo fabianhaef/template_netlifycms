84f6072b3ce71680d62345575b18a0dee7cfba3a
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-08-23 21:33:52 +0200

    removed slug

--- src/pages/bilder.js
+++ src/pages/bilder.js
@@ -4,14 +4,8 @@ import bilder from '../../public/data/bilder.json'
 
 export default function Bilder({ bilder = [] }) {
   return (
-    <Layout title="Gallerie">
-      <h1 className="title">Gallerie</h1>
-      {bilder.map(bild => (
-        < div className="box" key={bild.title}>
-          <img src={bild.coverImage} alt={bild.title} width="256" height="256"></img>
-        </div>
-      ))
-      }
+    <Layout title="Gallerie" className="container">
+
     </Layout >
   )
 }
