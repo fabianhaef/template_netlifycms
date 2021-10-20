c9a23eaf5db78a0c5b6426508d3f4eeac0605f0d
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-10-20 17:58:00 +0200

    several changes

--- src/components/VorstandsMitglied.js
+++ src/components/VorstandsMitglied.js
@@ -9,6 +9,13 @@ function VorstandsMitglied({mitglied}) {
           <h3 className="title is-6">{mitglied.rolle}</h3>
           <h4 className="text is-6">{mitglied.adress}, {mitglied.place}</h4>
           <h4 className="text is-6 mb-2">{mitglied.phone}</h4>
+          {mitglied.email.length !== 0 ? (
+            <h4 className="text is-6 mb-2">
+              <a href="mailto:${mitglied.email}" target="_blank" rel="noopener">{mitglied.email}</a>
+            </h4>
+          ) : (
+            <h4 className="text is-6 mb-2">Keine Email</h4>
+          )}
           <div dangerouslySetInnerHTML={{ __html: mitglied.bodyHtml }} />
         </div>
         <img className="image" src={mitglied.coverImage} alt={mitglied.title} width="128"></img>
