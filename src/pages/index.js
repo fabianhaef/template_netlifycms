c3c7c1df2b6e3d24c14bf5d2aaa88f36de8353b8
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-11-13 12:33:33 +0100

    meta description test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,15 @@ import Layout from '../components/Layout'
 import Tiles from '../components/Tiles'
 import Fest from '../components/Fest'
 
+import Head from 'next/head'
+
 
 export default function Home() {
   return (
     <Layout title="Home">
+      <Head>
+        <meta name="description" content="Herzlich Willkommen beim Schwingclub Zug und Umgebung" />
+      </Head>
       <section className="hero section-layout hero-mobile">
 
         <div className="container">
