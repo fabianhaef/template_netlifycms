ce575e435a02aaa0d30dc3688c11096cd66e9553
Fabian Haefliger <haefliger@hi-schweiz.ch>
2022-06-21 07:10:16 +0200

    new news article format

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,45 +1,55 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import news from "../../public/data/news.json";
 
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import news from '../../public/data/news.json'
-
-const defaultCoverImage = '/images/hero2.jpg' 
+const defaultCoverImage = "/images/hero2.jpg";
 
 export default function News({ news = [] }) {
-
-  const filteredNews = news.sort((a, b) => a.date - b.date)
+  const filteredNews = news.sort((a, b) => a.date - b.date);
   return (
     <Layout title="News">
       <section className="section">
         <div className="container">
           <h1 className="title mb-6">News</h1>
-          <h2 className="subtitle is-4 mb-4">Hier finden Sie alle relevanten News zum Schwingclub Zug und Umgebung.</h2>
+          <h2 className="subtitle is-4 mb-4">
+            Hier finden Sie alle relevanten News zum Schwingclub Zug und
+            Umgebung.
+          </h2>
           <div className="news-layout">
-            {filteredNews.map(post => (
-                <Link href="/news/[slug]" as={`/news/${post.slug}`} key={post.title}>
-                  <div className="news-article make-it-slow bg-primary-10 box" >
+            {filteredNews.map((post) => (
+              <Link
+                href="/news/[slug]"
+                as={`/news/${post.slug}`}
+                key={post.title}
+              >
+                <div className="news-article make-it-slow bg-primary-10 box">
+                  <div className="news-content">
                     <div className="news-title">
-                        <h2 className="title is-4 mr-4">{post.title}</h2>
-                        <p>{(post.dateFormatted)}</p>
-                      </div>
+                      <h2 className="title is-4 mr-4">{post.title}</h2>
+                      <p>{post.dateFormatted}</p>
+                    </div>
                     <div dangerouslySetInnerHTML={{ __html: post.bodyHtml }} />
-                    <img className="image news-image" src={post.coverImage || defaultCoverImage} alt={post.title} alt={post.title}></img>
                   </div>
-                </Link>
 
-
+                  <div className="image-wrapper"><img
+                    className="image news-image"
+                    src={post.coverImage || defaultCoverImage}
+                    alt={post.title}
+                  ></img></div>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
   return {
     props: {
-      news
-    }
-  }
+      news,
+    },
+  };
 }
--- src/pages/news/[slug].js
+++ src/pages/news/[slug].js
@@ -2,19 +2,21 @@ import Layout from '../../components/Layout'
 import news from '../../../public/data/news.json'
 
 export default function NewsPost({ post }) {
-  
+
   return (
     <Layout title={`${post.title}`} >
       <section className="section">
         <div className="container">
-          <div className="box">
-            <div className="news-title">
-              <h2 className="subtitle is-4 mb-4 mr-4">{post.title}</h2>
-              <p>{post.dateFormatted}</p>
-            </div>
+          <div className="news-article box">
             <div className="news-content">
+              <div className="news-title">
+                <h2 className="subtitle is-4 mb-4 mr-4">{post.title}</h2>
+                <p>{post.dateFormatted}</p>
+              </div>
               <div className="content" dangerouslySetInnerHTML={{ __html: post.bodyHtml }} />
-              <img src={post.coverImage} alt={post.title} className="news-content-image"/>
+            </div>
+            <div className="image-wrapper">
+              <img src={post.coverImage} alt={post.title} className="news-content-image" />
             </div>
           </div>
         </div>
